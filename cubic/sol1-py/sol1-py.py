b49e487a8c2767b6f57a360b8c96e8cb2c395c5b
wasd314 <68897049+wasd314@users.noreply.github.com>
2025-03-14 10:21:18 +0900

    add sol1

---
+++ cubic/sol1-py/sol1-py.py
@@ -0,0 +1,33 @@
+#! python
+
+def solve(n: int):
+    cube = []
+    for i in range(n + 1):
+        if i * i * i <= n:
+            cube.append(i * i * i)
+        else:
+            break
+    # cube[i] = i**3
+
+    c = len(cube)
+    acc = [0] * (c + 1)
+    for i in range(c):
+        acc[i + 1] = acc[i] + cube[i]
+    # acc[i] = sum(j**3 for j < i)
+    # acc[i + 1] = sum(j**3 for j <= i)
+
+    right = {acc[i + 1]: i for i in range(1, c)}
+    ans = []
+    for l in range(1, c + 1):
+        r = right.get(acc[l] + n, -1)
+        if r != -1:
+            ans.append((l, r))
+    return ans
+
+if __name__ == "__main__":
+    n = int(input())
+    ans = solve(n)
+    ans.sort()
+    for t in ans:
+        print(*t)
+
