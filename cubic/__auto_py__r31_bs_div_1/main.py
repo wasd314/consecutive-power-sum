6437a1b39f2e8d492a365c0e078c4ec071ccf2d8
wasd314 <68897049+wasd314@users.noreply.github.com>
2025-03-28 07:09:58 +0900

    gnerated solutions

---
+++ cubic/__auto_py__r31_bs_div_1/main.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+import sys
+from pathlib import Path
+sol = Path(__file__).resolve().parents[2] / "solution_py"
+sys.path.append(str(sol))
+
+import e3
+import utility
+
+if __name__ == "__main__":
+    utility.answer_cubic_with(e3.r31_bs_div_1)
