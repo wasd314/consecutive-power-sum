575dc1ce107550da775d6a761bb06b47d0a7664c
wasd314 <68897049+wasd314@users.noreply.github.com>
2025-03-30 21:05:06 +0900

    add cpp utility

---
+++ solution_cpp/e3.hpp
@@ -0,0 +1,100 @@
+#ifndef CONSECUTIVE_POWER_SUM_E3
+#define CONSECUTIVE_POWER_SUM_E3 1
+
+#include <algorithm>
+#include <concepts>
+#include <functional>
+#include <iostream>
+#include <ranges>
+#include <tuple>
+#include <vector>
+
+#include "utility.hpp"
+
+namespace wasd314::e3
+{
+    std::vector<solution_t> r30_two_pointer_(lint n, int)
+    {
+        using std::vector;
+        vector<lint> cube;
+        for (lint i = 0;; ++i) {
+            lint i3 = i * i * i;
+            if (i3 > n) break;
+            cube.push_back(i3);
+        }
+        int c = cube.size();
+        vector<solution_t> ans;
+
+        int r = 1;
+        lint current_sum = 0;
+        for (int l = 1; l < c; ++l) {
+            while (r < c && current_sum + cube[r] <= n) {
+                current_sum += cube[r];
+                r++;
+            }
+            if (current_sum == n) {
+                ans.emplace_back(3, l, r - 1);
+            }
+            if (l < r) {
+                current_sum -= cube[l];
+            }
+        }
+        std::ranges::sort(ans);
+        return ans;
+    }
+    auto r30_two_pointer = named_solver(r30_two_pointer_, "r30_two_pointer");
+
+    template <bool only_div, bool from_prev>
+    auto r31_bs()
+    {
+        using namespace std::literals::string_literals;
+
+        auto f = [=](lint n, int) {
+            using std::vector;
+            vector<solution_t> ans;
+            lint prev_l = n;
+            for (lint w = 1;; ++w) {
+                if (power_sum(3, 1, w + 1) > n) break;
+                if (only_div && (4 * n % w) != 0) continue;
+                lint l;
+                if (from_prev) {
+                    lint dl = 1;
+                    while (dl < prev_l && power_sum(3, prev_l - dl, prev_l - dl + w) >= n) {
+                        dl <<= 1;
+                    }
+                    l = bisect_left(std::max(0ll, prev_l - dl), prev_l + 1, [&](lint li) { return power_sum(3, li, li + w) >= n; });
+                    prev_l = l;
+                } else {
+                    lint r = 1;
+                    while (power_sum(3, r, r + w) <= n) {
+                        r <<= 1;
+                    }
+                    l = bisect_left(0, r + 1, [&](lint li) { return power_sum(3, li, li + w) >= n; });
+                }
+                if (power_sum(3, l, l + w) == n) {
+                    ans.emplace_back(3, l, l + w - 1);
+                }
+            }
+            std::ranges::reverse(ans);
+            // std::ranges::sort(ans);
+            return ans;
+        };
+        auto name = "r31_bs_"s + (only_div ? "div"s : "all"s) + (from_prev ? "pre"s : "1"s);
+        return named_solver(f, name);
+    }
+    auto r31_bs_all_1 = r31_bs<false, false>();
+    auto r31_bs_div_1 = r31_bs<true, false>();
+    auto r31_bs_all_pre = r31_bs<false, true>();
+    auto r31_bs_div_pre = r31_bs<true, true>();
+
+    std::vector<wrapped_solver> solvers{
+        r30_two_pointer,
+        r31_bs_all_1,
+        r31_bs_div_1,
+        r31_bs_all_pre,
+        r31_bs_div_pre,
+    };
+}  // namespace wasd314::e3
+
+
+#endif  // CONSECUTIVE_POWER_SUM_E3
---
+++ solution_cpp/utility.hpp
@@ -0,0 +1,111 @@
+#ifndef CONSECUTIVE_POWER_SUM_UTILITY
+#define CONSECUTIVE_POWER_SUM_UTILITY 1
+
+#include <concepts>
+#include <iostream>
+#include <numeric>
+#include <ranges>
+#include <tuple>
+#include <vector>
+
+namespace wasd314
+{
+    using lint = long long;
+    using solution_t = std::tuple<int, lint, lint>;
+
+    struct named_solver_base {
+        std::string name;
+        virtual std::vector<solution_t> operator()(lint n, int e) const
+        {
+            std::cerr << "virtual function called(" << n << "," << e << ")" << std::endl;
+            return {};
+        }
+        const named_solver_base& get() const { return *this; }
+    };
+    template <typename T>
+    struct named_solver : named_solver_base {
+        T f;
+        named_solver(T f, std::string name) : f(f) { this->name = name; }
+        std::vector<solution_t> operator()(lint n, int e) const override { return f(n, e); }
+    };
+    using wrapped_solver = std::reference_wrapper<named_solver_base>;
+    // 本体の関数 f の型 T の差異を named_solver_base で吸収し，
+    // std::reference_wrapper に包むことで f を実行している
+
+    // e-th power sum of [l, r) or [0, l)
+    __int128_t power_sum(int e, __int128_t l, __int128_t r = -10)
+    {
+        if (r != -10) {
+            return power_sum(e, r) - power_sum(e, l);
+        }
+        if (e == 1) {
+            return l * (l - 1) / 2;
+        } else if (e == 2) {
+            return l * (l - 1) * (2 * l - 1) / 6;
+        } else if (e == 3) {
+            return l * (l - 1) / 2 * l * (l - 1) / 2;
+        } else if (e == 4) {
+            return l * (l - 1) * (2 * l - 1) * (3 * l * l - 3 * l - 1) / 30;
+        } else if (e == 5) {
+            return (l - 1) * l / 2 * (l - 1) * l / 2 * (2 * (l - 1) * l - 1) / 3;
+        } else {
+            return 0;
+        }
+    }
+
+    void answer_cubic_with(const wrapped_solver& f)
+    {
+        using std::cout;
+        using std::endl;
+
+        lint n;
+        std::cin >> n;
+        std::vector<solution_t> ans = f(n, 3);
+        cout << ans.size() << endl;
+        for (auto [e, l, r] : ans) {
+            cout << l << ' ' << r << endl;
+        }
+    }
+
+    template <int E_FIRST>
+    requires(E_FIRST == 1 || E_FIRST == 2)
+    void answer_power_with(const std::vector<wrapped_solver>& sols)
+    {
+        lint n;
+        std::cin >> n;
+        std::vector<solution_t> ans;
+
+        int count = sols.size();
+        for (int i = 0; i < count - 1; ++i) {
+            auto ansi = sols[i](n, E_FIRST + i);
+            ans.insert(ans.end(), ansi.begin(), ansi.end());
+        }
+        for (int e = E_FIRST + count - 1; e < 64; ++e) {
+            auto ansi = sols[count - 1](n, e);
+            ans.insert(ans.end(), ansi.begin(), ansi.end());
+        }
+        std::cout << ans.size() << std::endl;
+        for (auto [e, l, r] : ans) {
+            std::cout << e << ' ' << l << ' ' << r << std::endl;
+        }
+    }
+
+    template <typename T>
+    requires requires(T pred, lint l) {
+        { pred(l) } -> std::same_as<bool>;
+    }
+    lint bisect_left(lint lo, lint hi, T pred)
+    {
+        while (lo + 1 < hi) {
+            lint mi = std::midpoint(lo, hi);
+            if (pred(mi)) {
+                hi = mi;
+            } else {
+                lo = mi;
+            }
+        }
+        return hi;
+    }
+}  // namespace wasd314
+
+#endif  // CONSECUTIVE_POWER_SUM_UTILITY
