1bc58f8a8cc3645233b606eee6f5a7fad27c5fb1
wasd314 <68897049+wasd314@users.noreply.github.com>
2025-04-02 14:44:03 +0900

    list e >= 2 solvers

---
+++ solution_py/e2.py
@@ -0,0 +1,14 @@
+import ee
+
+solvers_ac = [
+    (2, ee.re1[0], ee.re0_two_pointer),
+    (2, ee.re1[1], ee.re0_two_pointer),
+    (2, ee.re1[2], ee.re0_two_pointer),
+    (2, ee.re1[3], ee.re0_two_pointer),
+]
+
+solvers_tle = [
+    (2, ee.re0[0], ee.re0_two_pointer),
+    (2, ee.re0[1], ee.re0_two_pointer),
+    (2, ee.re0[2], ee.re0_two_pointer),
+]
