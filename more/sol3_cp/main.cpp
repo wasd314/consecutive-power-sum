36f7e052671b6816419982b676bd67e241f6141c
wasd314 <68897049+wasd314@users.noreply.github.com>
2025-04-08 23:07:08 +0900

    add reference cpp SOLUTION

---
+++ more/sol3_cp/main.cpp
@@ -0,0 +1,9 @@
+#include "e1.hpp"
+#include "ee.hpp"
+#include "utility.hpp"
+
+int main()
+{
+    using namespace wasd314;
+    answer_power_with<1>({e1::r13_pe, ee::re1[3], ee::re1[3], ee::re0[0]});
+}
