a1c219c480b04829ff0cea75e1d357bd3ef139b8
wasd314 <68897049+wasd314@users.noreply.github.com>
2025-08-15 22:40:00 +0900

    cubic: follow new file name format

--- cubic/tests/generator.py
+++ cubic/tests/generator.py
@@ -16,33 +16,33 @@ MAX_N = 10**18
 # max c s.t. c**3 <= MAX_N
 c = bs.bisect_right(range(MAX_N), MAX_N, key=lambda x: x**3) - 1
 
-def gen_reciprocal():
+def gen_reciprocal(min_n: int, max_n: int):
+    """一様乱択よりも（大雑把に）小さい数の出現確率を上げて乱択"""
     while True:
         n = random.randrange(0, 1 << 60) >> random.randrange(0, 60)
-        if MIN_N <= n <= MAX_N:
+        if min_n <= n <= max_n:
             return n
 
-def gen_have_sol():
+def gen_have_sol(min_n: int, max_n: int):
     while True:
         l = random.randint(1, c)
         r = random.randint(1, c) + 1
         n = utility.power_sum(3, l, r)
-        if l <= r and MIN_N <= n <= MAX_N:
+        if l <= r and min_n <= n <= max_n:
             return n
 
 def gen_cases(num: int, f):
     return [f() for _ in range(num)]
 
+def number_of_solutions(n: int):
+    return len(ee.re1_bs_div_1(n, 3))
+
 def dump_cases(prefix, cases: list, sort=True):
     if sort:
-        cases = sorted(cases)
+        cases = sorted(cases, key=lambda n: (number_of_solutions(n), n))
     for i, n in enumerate(cases):
         with open(f"{prefix}-{i:02}.in", "w") as f:
             print(n, file=f)
-
-dump_cases("001-random", gen_cases(15, gen_have_sol))
-dump_cases("002-random", gen_cases(15, gen_reciprocal))
-
 
 # small
 handmade = [1, 2, 5, 7, 8, 9]
@@ -56,11 +56,8 @@ handmade.extend([999999_999999_999967, 999999_999999_999989])
 # semiprime
 handmade.extend([999999_999999_999787, 999999_999999_999541])
 
-sol_count = {n: len(ee.re1_bs_div_1(n, 3)) for n in handmade}
-cases = [[] for _ in range(max(sol_count.values()) + 1)]
-for n, l in sol_count.items():
-    cases[l].append(n)
-for l, ns in enumerate(cases):
-    dump_cases(f"1{l:02}-handmade", ns)
+dump_cases("10-handmade", handmade)
 
+dump_cases("11-random", gen_cases(15, lambda: gen_have_sol(MIN_N, MAX_N)))
+dump_cases("12-random", gen_cases(15, lambda: gen_reciprocal(MIN_N, MAX_N)))
 
